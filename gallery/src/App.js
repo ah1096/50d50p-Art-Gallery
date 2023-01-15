ccafd219871a8a4cba678d946d1800567d7cde5e
ah1096 <alyssa.holbert@student.awesomeinc.org>
2023-01-15 15:57:50 +0000

    added blurry loading page

--- gallery/src/App.js
+++ gallery/src/App.js
@@ -1,23 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+const loadText = document.querySelector('.loading-text')
+const bg = document.querySelector('.bg')
+const caption = document.querySelector('.caption')
+
+let load = 0
+let int = setInterval(blurring, 30)
+    // ^^ runs the blurring function every 30 milliseconds
+
+function blurring() {
+    load++
+
+    if (load > 99) {
+        clearInterval(int)
+            // ^^ stops the count from running forever
+        caption.style.visibility = `visible`;
+    }
+
+    loadText.innerText = `${load}%`
+    loadText.style.opacity = scale(load, 0, 100, 1, 0)
+        // ^^ makes text become transparent w/ load Number. rever 1 and 0 for opposite effect
+    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+        // ^^ makes image blur change from 30 px to 0px
+    
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max
+    - in_min) + out_min;
+}
+    // ^^ maps the opacity to the load number
+
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section class="bg"></section>
+      <div class="loading-text">0%</div>
+      <button class="caption"><a href="#" target="_blank"> enter the museum </a></button>
     </div>
   );
 }
